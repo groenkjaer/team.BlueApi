cb4deb5af3c8c35bc13969b54859a6792d4b5f18
groenkjaer <FillipJensen@live.dk>
2022-10-02 21:24:07 +0200

    added api controller

---
+++ Controllers/TextController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using team.BlueApi.Models;
+
+namespace team.BlueApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TextController : ControllerBase
+    {
+        private readonly WordsContext _context;
+        public TextController(WordsContext ctx)
+        {
+            _context = ctx;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Words>>> Get()
+        {
+            return await _context.Words.ToListAsync();
+        }
+    }
+}
