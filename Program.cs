fa617245e3404ea325b72905b35f6996fab134db
groenkjaer <FillipJensen@live.dk>
2022-10-02 21:03:55 +0200

    added wordscontext to services

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using team.BlueApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<WordsContext>(options => options.UseSqlServer());
 
 var app = builder.Build();
 
