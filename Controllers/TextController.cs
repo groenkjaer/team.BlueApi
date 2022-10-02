6f7ec69d30d2c47d6be6b63a794792004825e1b6
groenkjaer <FillipJensen@live.dk>
2022-10-02 22:54:13 +0200

    added model specific for receiving input

--- Controllers/TextController.cs
+++ Controllers/TextController.cs
@@ -21,5 +21,26 @@ namespace team.BlueApi.Controllers
         {
             return await _context.Words.ToListAsync();
         }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody]ReceivedWords receivedWords)
+        {
+            if (receivedWords.Text == null)
+                return BadRequest();
+
+            // Ignore case and remove nonletter characters
+            string recWordsCleaned = new (receivedWords.Text.ToLower().Where(c => !char.IsPunctuation(c)).ToArray());
+
+            List<string>? distinctWords = recWordsCleaned.Split(" ", StringSplitOptions.RemoveEmptyEntries).Distinct().ToList();
+
+            // Remove already persisted words from list
+            distinctWords.RemoveAll(s => _context.Words.Any(entity => entity.Text == s));
+
+            // Add the remaining strings as Word objects
+            await _context.Words.AddRangeAsync(distinctWords.Select(word => new Words() { Text = word }));
+            await _context.SaveChangesAsync();
+
+            return Ok(distinctWords.Count);
+        }
     }
 }
