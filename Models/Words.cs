fb6dc049018b08eadead1f8a37fd9adfb739028c
groenkjaer <FillipJensen@live.dk>
2022-10-02 20:59:11 +0200

    added model, with overrides

---
+++ Models/Words.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace team.BlueApi.Models
+{
+    public class Words
+    {
+        public string? Text { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Words words &&
+                   Text == words.Text;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Text);
+        }
+    }
+}
