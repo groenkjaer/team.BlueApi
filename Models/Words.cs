850a8f2404aaa66dbdfbff9dbf8b509e459f7560
groenkjaer <FillipJensen@live.dk>
2022-10-02 21:24:35 +0200

    text has a maxlength 100 now

--- Models/Words.cs
+++ Models/Words.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace team.BlueApi.Models
 {
     public class Words
     {
+        [Key]
+        [MaxLength(100)]
         public string? Text { get; set; }
 
         public override bool Equals(object? obj)
