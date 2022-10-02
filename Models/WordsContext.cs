09712cef1379ce547804a7eba61274e76f567816
groenkjaer <FillipJensen@live.dk>
2022-10-02 21:03:37 +0200

    added DbContext for Words

---
+++ Models/WordsContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace team.BlueApi.Models
+{
+    public class WordsContext : DbContext
+    {
+        public WordsContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Words>? Words { get; set; }
+    }
+}
